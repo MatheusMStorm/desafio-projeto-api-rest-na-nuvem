07fe596cbfe3efb946b4ca6ca3a782f6fb3e73b3
matheusmstorm <matheus.moura0308@gmail.com>
2024-07-22 08:05:21 -0300

    first commit

---
+++ src/main/java/com/storm/desafio_projeto_api_rest_na_nuvem/DesafioProjetoApiRestNaNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.storm.desafio_projeto_api_rest_na_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoApiRestNaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoApiRestNaNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storm/desafio_projeto_api_rest_na_nuvem/DesafioProjetoApiRestNaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storm.desafio_projeto_api_rest_na_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoApiRestNaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
